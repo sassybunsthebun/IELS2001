8ccb412c856215d6cd5f180846dc87a500299491
sassybunsthebun <sara.batevik@epost.no>
2024-04-22 12:13:24 +0200

    added cpp and h files to zumobil folder

---
+++ zumobil/Prosjekt.h
@@ -0,0 +1,12 @@
+#ifndef Prosjekt_h
+#define Prosjekt_h
+
+#include "Arduino.h"
+#include <PubSubClient.h>
+
+  void connectWiFi(const char* ssid, const char* password);
+  void sendWhatsAppMessage(String& message, String& phoneNumber, String& apiKey);
+  void reconnectMQTT(PubSubClient& client);
+  void wireTransmit(byte zumoaddress, byte kjoremodus);
+
+#endif
