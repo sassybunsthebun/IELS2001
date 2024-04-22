08d2f38f22759e5a747d8f10bcba2bcc08c9b57f
sassybunsthebun <sara.batevik@epost.no>
2024-04-22 13:24:52 +0200

    Added line follower function

--- zumobil/zumobil.ino
+++ zumobil/zumobil.ino
@@ -1,16 +1,35 @@
 #include <Wire.h>
+#include <Zumo32U4Encoders.h>
+#include <Zumo32U4.h>
 
-///VARIABLES FOR READING THE CAR SENSORS///
+/// DEFINE THE INSTANCES OF THE CLASSES IN THE ZUMO32u4 LIBRARY ///
+Zumo32U4LineSensors lineSensors;
+Zumo32U4Motors motors;
+Zumo32U4OLED display;
+Zumo32U4Encoders encoder;
+
+/// VARIABLER FOR LINE FOLLOWING ///
+int speed = 220; 
+unsigned long previousMillis = 0; 
+
+#define NUM_SENSORS 5 // amount of sensors
+unsigned int lineSensorValues[NUM_SENSORS]; //creates an array lineSensorValues with a length of NUM_SENSORS
+
 void setup()
 {
   Wire.begin(4);                // join i2c bus with address #4
-  Wire.onReceive(receiveEvent); // register event
+  Wire.onReceive(receiveEvent); 
   Serial.begin(9600);
+  lineSensors.initFiveSensors(); //merges the line sensors 
+  motors.setSpeeds(-100,100); 
+  for (byte i = 0; i <= 254; i++){
+    lineSensors.calibrate();
+  }
 }
 
 void loop()
 {
-
+  linjefolging();
 }
 
 // function that executes whenever data is received from master
@@ -29,7 +48,26 @@ void receiveEvent(int howMany)
 
 //A function that follows a taped up line on the floor using the Zumo32u4's linefollower sensors
 void linjefolging(){
-  //sett inn funskjonen for linjefølging her 
-}
+  int previousOffset;
+  int maxSpeed = speed; 
+  int sensorValue = lineSensors.readLine(lineSensorValues);//measures a value between 0 and 4000
+  int offset; //offset from 2000
+  
+  if (sensorValue >= 2020){ 
+    offset = (sensorValue - 2000)/5;//the bigger the offset, the shorter the turn of the car 
+    maxSpeed -= offset/2;
+    unsigned int leftMotor = maxSpeed + offset - previousOffset/4;
+    unsigned int rightMotor = maxSpeed - offset + previousOffset/4;
+    motors.setSpeeds( leftMotor, rightMotor);
+  }
+  else if (sensorValue <= 1980) { 
+    offset = (2000 - sensorValue)/5;
+    
+    maxSpeed -= offset/2;
 
-//sett inn funksjoner (void loops) som leser av de ulike sensorverdiene 
+    unsigned int leftMotor = maxSpeed - offset + previousOffset/4;
+    unsigned int rightMotor = maxSpeed + offset - previousOffset/4;
+    motors.setSpeeds(leftMotor, rightMotor);
+  }
+  previousOffset = offset;
+} 
