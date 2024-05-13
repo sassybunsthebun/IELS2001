c8e0695fd2471c8e8ef2ca35f8aae805505e643d
EmmaWu419 <145002197+EmmaWu419@users.noreply.github.com>
2024-05-13 11:54:58 +0200

    Add files via upload
    krapp sving

---
+++ zumobil/i2c_esp32_slave.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+
+//give device adress between 0 and 127 (apperently 0-7 are occupied)  gir enheten en adresse 
+int espAddress = 4;
+
+//antall datapoints
+const int numReadings = 6;
+
+//beskriver hva de tallene i listen betyr
+int right = 2;
+int left = 0;
+int null = 1;
+
+int krappSving[numReadings];
+
+//funksjon som avleser innkommende data
+void receiveEvent(int howMany){
+  while(Wire.available()){
+    for(int i =0; i < howMany; i++){
+      krappSving[i] = Wire.read();
+      Serial.println(krappSving[i]);
+    }
+  }
+}
+
+void setup() {
+  //join i2c bus with address #4
+  Wire.begin(espAddress);
+  //registers a function to be called when peripheral device recieves a transmission from master device
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+}
